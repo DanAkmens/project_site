0bc79f56c52acc91bd255a4b81446e9bfd45a22f
piladzisi <piladzisi@gmail.com>
2018-12-19 01:34:32 +0000

    Arlington DB
    Not sure if exported correctly

--- arlington_test.sql
+++ arlington_test.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2018 at 02:03 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- Generation Time: Dec 19, 2018 at 02:33 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -47,12 +47,12 @@ CREATE TABLE `jobs` (
 INSERT INTO `jobs` (`id`, `sector_name`, `title`, `description`, `salary_min`, `salary_max`, `type`, `city`, `created`) VALUES
 (6, NULL, 'Driver', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ', '15000', NULL, NULL, 'Hull', '2018-11-27 12:00:55'),
 (7, NULL, 'Head Nurse', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ', '24000', NULL, NULL, 'Hull', '2018-11-27 12:00:55'),
-(11, NULL, 'Blade Runner', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ', '50000', NULL, NULL, 'New York', '2018-11-27 12:00:55'),
-(39, NULL, 'waiter', '<p>If you have strong legs, come work for us.</p>', '14000', '24000', NULL, 'Hoboken', '2018-11-30 20:25:31'),
-(40, NULL, 'PHP developer', '<p>PerfectÂ job for developer</p>', '55000', NULL, NULL, 'London', '2018-12-01 19:11:45'),
-(42, '1', 'Accountant', '<p>Accountant with experience, 2 - 5 years.</p>', '20000', NULL, 'full-time', 'Hull', '2018-12-11 22:30:28'),
-(43, '8', 'Web developer', '<p>CSS Boot Strap JavaScript</p>', '30000', NULL, 'part-time', 'Hull', '2018-12-11 22:32:48'),
-(44, '2', 'Engineer', '<p>Software Solid Works, Experience 3 years,&nbsp;responsible. Ability to learn quickly, Iron Man. Capital &pound;3 000 000. Unmarried, no kids, Military service considered an advantage. Speak 3 language fluently. Runner, nonsmoker, positive attitude. If you have those skills and more you are welcome to apply.&nbsp;</p>', '5000', '6000', 'full-time', 'Greensby', '2018-12-18 11:27:03');
+(11, NULL, 'Manager', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ', '18000', '25000', NULL, 'New York', '2018-11-27 12:00:55'),
+(39, NULL, 'Head of Department', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', '14000', '24000', NULL, 'Hoboken', '2018-11-30 20:25:31'),
+(40, NULL, 'PHP developer', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', '55000', NULL, NULL, 'London', '2018-12-01 19:11:45'),
+(42, '1', 'Accountant', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', '20000', NULL, 'full-time', 'Hull', '2018-12-11 22:30:28'),
+(43, '8', 'Web developer', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', '30000', '40000', 'part-time', 'Hull', '2018-12-11 22:32:48'),
+(44, '2', 'Engineer', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', '5000', '6000', 'full-time', 'Greensby', '2018-12-18 11:27:03');
 
 -- --------------------------------------------------------
 
@@ -74,14 +74,10 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `body`, `author`, `post_image`, `created_at`) VALUES
-(1, 'Mars is Closer than you think', 'The space shuttle close commander.Â  Prepare laser guns, the enemy will pay for our friends.', 'Dan Strong', 'uni.jpg', '2018-11-27 21:40:47'),
-(4, 'The bright future of Robotics', 'Hello iRobot, we are happy about you. Thank you, master, said shiny robot.', 'Master John Doe', '61.jpg', '2018-11-28 00:41:32'),
-(5, 'Candidates Attraction - why is it so important?', '<p>Because, because we are. They are, not because. Candidates are strong, smart, yes, no.</p>', 'Dragon Snout', '61.jpg', '2018-11-28 00:55:59'),
-(6, 'Turnover is vanity, profit is sanity, but cash flow is king', '<p>yes no yes no yes no yes no yes no yes no yes no yes no yes no yes no</p>', 'Crazy Frog', '510225-PI34BN-784.jpg', '2018-11-28 01:28:44'),
-(8, 'Only tonight the moon is Red', '<p>Moon is rad what a phenomena</p>', 'Simple Dude', '61.jpg', '2018-11-29 21:48:28'),
-(9, 'Hello', 'To be or not to be! Give me my money and you can be or not, said angrily Dragon.  ', 'Bigy.', '645.jpg', '2018-12-01 19:09:45'),
-(10, 'some post', '<p>Some post</p>', 'Dragon Big', '510225-PI34BN-784.jpg', '2018-12-01 19:14:29'),
-(13, 'The Palms', 'When I see Palm Trees, I plunge into the dream, where I swimÂ in the blue Ocean with dolphins. You are crazy man, dolphins will eat you alive, they are predators. Not they are not said old man, they are our friends the same as dogs.', 'Tatu Frog', 'Dainis-2.jpg', '2018-12-12 13:42:44');
+(1, 'These 5 Trends Shaped the 2018 Job Market', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 'Mary Joe', '', '2018-12-18 00:00:00'),
+(4, 'The bright future of Robotics', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 'John Doe', '61.jpg', '2018-11-28 00:00:00'),
+(5, 'Candidates Attraction - why is it so important?', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 'Mary Joe', '61.jpg', '2018-11-28 00:55:59'),
+(6, 'Turnover is vanity, profit is sanity, but cash flow is king', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 'John Doe', '510225-PI34BN-784.jpg', '2018-11-28 01:28:44');
 
 -- --------------------------------------------------------
 
